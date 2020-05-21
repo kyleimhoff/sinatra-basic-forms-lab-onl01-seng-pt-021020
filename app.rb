f887415e6dd2cb8858904308488d55b85dc56b74
Kyle Imhoff <imhoff.kyle@gmail.com>
2020-05-21 04:49:38 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -8,7 +8,7 @@ end
 get '/create_puppy' do 
   erb :create_puppy
 end
-post '/create_puppy'
+post '/puppy'
 @puppy = Puppy.new(params['name'], params['breed'], params['months_old'])
 erb :display_puppy 
 end 
