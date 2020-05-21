5ddaccb5d4bb2e3e1ad5003c323bbd825ddca106
Kyle Imhoff <imhoff.kyle@gmail.com>
2020-05-21 04:41:36 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,8 +1,12 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
-get "/" do 
+get '/' do 
   
   erb :index 
 end
+get '/create_puppy' do 
+  erb :create_puppy
+end
+post '/create_puppy'
 end
